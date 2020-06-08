eda0ab5559edfe64af256e7ffa5e103270247dcc
Ckrendel <danich1993@mail.ru>
2020-06-08 19:04:35 +0100

    show locanion and time on home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,33 +1,58 @@
 import 'package:flutter/material.dart';
 
-
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   Map data = {};
 
   @override
   Widget build(BuildContext context) {
-
     data = ModalRoute.of(context).settings.arguments;
     // print(data);
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            FlatButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+          child: Column(
+            children: <Widget>[
+              FlatButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text('Edit Location'),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    data['location'],
+                    style: TextStyle(
+                      fontSize: 30,
+                      letterSpacing: 2,
+                      color: Colors.blueGrey[900],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                data['time'],
+                style: TextStyle(
+                  fontSize: 66,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
